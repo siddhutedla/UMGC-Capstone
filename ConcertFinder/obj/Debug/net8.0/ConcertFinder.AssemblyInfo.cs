1b2448af8c095726d2daf994afd4a82fc0c30975
“Kimball” <8474541+Kimballs@users.noreply.github.com>
2024-04-22 15:16:23 -0700

    Improved changes

--- ConcertFinder/obj/Debug/net8.0/ConcertFinder.AssemblyInfo.cs
+++ ConcertFinder/obj/Debug/net8.0/ConcertFinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConcertFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a07e3a132214e42eda73c78b52055cf4543093")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890bb8fa88e45b8159b70c98c0edf64b256d3132")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConcertFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConcertFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
